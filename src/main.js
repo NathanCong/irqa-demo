fd4cf6479728eb8d7fe0336b7bc5b72011bd1396
NathanCong <congyunan@xdf.cn>
2025-06-17 13:56:54 +0800

    feat: 初始化项目

---
+++ src/main.js
@@ -0,0 +1,18 @@
+/**
+ * main.js 入口程序
+ */
+const { app, BrowserWindow } = require('electron');
+const { resolve } = require('path');
+
+const createWindow = () => {
+  const browserWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  browserWindow.loadFile(resolve(__dirname, './index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
