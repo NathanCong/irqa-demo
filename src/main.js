f1eceee93b4ee67488b9e7183299c66a51d7062d
NathanCong <congyunan@xdf.cn>
2025-06-17 17:06:17 +0800

    feat: 测试 windows 打包功能

--- src/main.js
+++ src/main.js
@@ -16,3 +16,9 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 });
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
